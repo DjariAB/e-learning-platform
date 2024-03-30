43562dc8453ac7f5c3c2fcb375f45c44e8898693
Djari <abdelbassetd9@gmail.com>
2024-03-30 18:41:37 +0100

    add course page - safe commit

--- src/app/courses/[Id]/page.tsx
+++ src/app/courses/[Id]/page.tsx
@@ -1,32 +1,50 @@
 import CourseLevel from "@/components/courselevel";
-import MainNavBar from "@/components/mainNavbar"
+import MainNavBar from "@/components/mainNavbar";
 import { db } from "@/server/db";
 import { courseTable } from "@/server/db/schema";
 
-export default async function CoursePage({ params }: { params: { Id: string } }) {
+export default async function CoursePage({
+  params,
+}: {
+  params: { Id: string };
+}) {
   const courses = await db.select().from(courseTable);
   const course = courses[0];
 
   return (
-    <div >
+    <div className="bg-blue-800">
       <MainNavBar />
-      
-      <div>
-        <img src="" alt="" />
-        <div><h1>Course title</h1>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Voluptates, accusamus labore quo natus, ipsa sint at perspiciatis
-              tempore impedit, assumenda nisi. Dolor, harum atque facere eaque
-              maiores esse rem vero!
-            </p>
-            <div>
-              <CourseLevel
-                level={course!.level.toString()}
-                additionalStyle="text-black"
-              /></div>
+
+      <div className="flex items-center gap-20 px-24 py-16">
+        <img
+          src="https://miro.medium.com/v2/resize:fit:2000/1*y6C4nSvy2Woe0m7bWEn4BA.png"
+          alt="Course image"
+          className="size-96 rounded-2xl object-cover"
+        />
+        <div className="flex flex-col gap-6 text-white">
+          <h1 className="text-4xl font-medium">Course title</h1>
+          <p className="text-2xl font-light">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates,
+            accusamus labore quo natus, ipsa sint at perspiciatis tempore
+            impedit, assumenda nisi. Dolor, harum atque facere eaque maiores
+            esse
+          </p>
+          <div>
+            <CourseLevel
+              level={course!.level.toString()}
+              additionalStyle="text-black"
+            />
+          </div>
+          <div className="flex items-center gap-5">
+            <img
+              src="https://i.pinimg.com/736x/f1/50/2c/f1502cf311fc2652aba302f0513a2490.jpg"
+              className="size-10 rounded-full object-cover"
+              alt="educator's avatar"
+            />
+            <p>Educator name</p>
+          </div>
+        </div>
       </div>
-    </div>
     </div>
   );
 }
