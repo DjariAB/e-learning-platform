966833a6434dd439e169d272f74aa535c065c78d
Saif <faysalbakziz@gmail.com>
2024-04-05 14:41:12 +0100

    added lessonId route

--- src/app/courses/[courseId]/[chapterId]/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function LessonPage({
-  params,
-}: {
-  params: { chapterId: string };
-}) {
-  return <div>this is chapter {params.chapterId} </div>;
-}
