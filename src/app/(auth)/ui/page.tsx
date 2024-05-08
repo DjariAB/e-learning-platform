8f22a91ac3100cc60b712d16f3b9ce5d225c7287
Saif <faysalbakziz@gmail.com>
2024-05-08 13:49:03 +0100

    added the AiquizzInput component

--- src/app/(auth)/ui/page.tsx
+++ src/app/(auth)/ui/page.tsx
@@ -1,10 +1,8 @@
 "use client";
 
 import { useState } from "react";
-import { continueConversationTest, Message } from "./actions";
+import { continueConversationTest, type Message } from "../../../lib/ai/actions";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import { Separator } from "@/components/ui/separator";
-import { Input } from "@/components/ui/input";
 import { Anek_Latin } from "next/font/google";
 import { Button } from "@/components/ui/button";
 
@@ -12,7 +10,7 @@ const anekLatin = Anek_Latin({
   subsets: ["latin"],
   weight: ["200", "300", "400", "500", "600"],
 });
-function Home() {
+export default function Home() {
   return (
     <div className={`text flex justify-center pt-6 ${anekLatin.className}`}>
       <Card className="w-fit rounded-xl">
@@ -22,7 +20,7 @@ function Home() {
           </CardTitle>
         </CardHeader>
         <br />
-        <CardContent className="flex flex-col  px-3  text-lg tracking-wider text-[#1E1E1E]">
+        <CardContent className="flex flex-col gap-5 px-3  text-lg tracking-wider text-[#1E1E1E]">
           <div className="flex gap-3  ">
             <div className="flex size-8 items-center justify-center rounded-full bg-blue-100 text-lg font-semibold text-blue-900">
               {" "}
@@ -35,7 +33,7 @@ function Home() {
                 value={"The state updater function"}
               />
               <div className="pr-5">
-                <br className="border border-border " />
+                {/* <hr className="border border-border " /> */}
               </div>
             </div>
           </div>
@@ -47,7 +45,7 @@ function Home() {
             <div className="flex grow flex-col gap-2  ">
               <input name="one" type="text" value={"The initial state"} />
               <div className="pr-5">
-                <br className="border border-border " />
+                {/* <br className="border border-border " /> */}
               </div>
             </div>
           </div>
@@ -59,7 +57,7 @@ function Home() {
             <div className="flex grow flex-col gap-2  ">
               <input name="one" type="text" value={"The initial state"} />
               <div className="pr-5">
-                <br className="border border-border " />
+                {/* <br className="border border-border " /> */}
               </div>
             </div>
           </div>
@@ -71,9 +69,13 @@ function Home() {
             <div className="flex grow flex-col gap-2  ">
               <input name="one" type="text" value={"The initial state"} />
               <div className="pr-5">
-                <br className="border border-border " />
+                {/* <br className="border border-border " /> */}
               </div>
             </div>
+          </div>
+
+          <div className="flex justify-center px-10">
+            <Button className="rounded-lg bg-blue-900 ">submit</Button>
           </div>
         </CardContent>
       </Card>
@@ -81,7 +83,7 @@ function Home() {
   );
 }
 
-export default function Test() {
+function Test() {
   const [conversation, setConversation] = useState<Message>();
   const [input, setInput] = useState<string>("");
 
---
+++ src/lib/ai/actions.tsx
@@ -0,0 +1,212 @@
+"use server";
+
+// import { Weather } from '@ai-studio/components/weather'
+import { generateText } from "ai";
+import { google } from "@ai-sdk/google";
+import { createStreamableUI } from "ai/rsc";
+import { type ReactNode } from "react";
+import { z } from "zod";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import { db } from "@/server/db";
+import { questionTable } from "@/server/db/schema";
+import { generateId } from "lucia";
+
+export interface Message {
+  role: "user" | "assistant";
+  content: string;
+  display?: ReactNode; // [!code highlight]
+}
+
+export async function continueConversationTest(question: string) {
+  const stream = createStreamableUI(); // [!code highlight]
+  // const res = await fetch(
+  //   "https://uploadthing-prod.s3.us-west-2.amazonaws.com/7e8cd3f4-7f86-4194-ada6-6228b43a21c5-pkoo3l.md",
+  // );
+
+  // const data = await res.text();
+
+  const { text, toolResults } = await generateText({
+    model: google("models/gemini-pro"),
+    system:
+      "You are a friendly quizz generator you are given a subject by the user and you give him a question with answsers about that subject ",
+    prompt: `generate a quizz about this ${question} `,
+    tools: {
+      generateQuiz: {
+        description: "generate a quiz about something",
+        parameters: z.object({
+          question: z
+            .string()
+            .describe(
+              "ask the user a question about the subject he chose make it hard",
+            ),
+          wronganswer: z.string().describe("Choice number one (wrong answer)"),
+          choice2: z.string().describe("Choice number four (wrong answer)"),
+          choice3: z.string().describe("Choice number three (wrong answer)"),
+          correctAnswer: z
+            .string()
+            .describe("choice number four (The correct answer)"),
+        }),
+        execute: async ({
+          question,
+          wronganswer,
+          correctAnswer,
+          choice2,
+          choice3,
+        }) => {
+          const que = question as string;
+          const wrong = wronganswer as string;
+          const wrong2 = choice2 as string;
+          const wrong3 = choice3 as string;
+          const correct = correctAnswer as string;
+          // const id = generateId(7);
+          // await db
+          //   .insert(questionTable)
+          //   .values({
+          //     question: que,
+          //     choice1: wrong,
+          //     choice2: wrong2,
+          //     choice3: wrong3,
+          //     correctAnswer: correct,
+          //     id
+          //   });
+          stream.done(
+            <StreamableUI
+              correctAnswer={correct}
+              question={que}
+              wronganswer={wrong}
+              wronganswer2={wrong2}
+              wronganswer3={wrong3}
+            />,
+          ); // [!code highlight]
+          return `${question}`; // [!code highlight]
+        },
+      },
+    },
+  });
+
+  return {
+    role: "assistant" as const,
+    content: text || toolResults.map((toolResult) => toolResult.result).join(),
+    display: stream.value, // [!code highlight]
+  };
+}
+
+export async function StreamableUI({
+  correctAnswer,
+  question,
+  wronganswer,
+  wronganswer2,
+  wronganswer3,
+}: {
+  question: string;
+  wronganswer: string;
+  wronganswer2: string;
+  wronganswer3: string;
+  correctAnswer: string;
+}) {
+  return (
+    <form action={quizAction}>
+      <Card className="w-1/2 rounded-xl">
+        <CardHeader className="">
+          <CardTitle className="  text-xl">
+            <input className="w-full" name="question" value={question} />
+          </CardTitle>
+        </CardHeader>
+        <br />
+        <CardContent className="flex flex-col gap-5 px-3  text-lg tracking-wider text-[#1E1E1E]">
+          <div className="flex gap-3  ">
+            <div className="flex size-8 items-center justify-center rounded-full bg-blue-100 text-lg font-semibold text-blue-900">
+              {" "}
+              1
+            </div>
+            <div className="flex grow flex-col gap-2  ">
+              <input
+                className="w-full"
+                name="choice1"
+                type="text"
+                value={wronganswer}
+              />
+              <div className="pr-5"></div>
+            </div>
+          </div>
+          <div className="flex gap-3  ">
+            <div className="flex size-8 items-center justify-center rounded-full bg-blue-100 text-lg font-semibold text-blue-900">
+              {" "}
+              2
+            </div>
+            <div className="flex grow flex-col gap-2  ">
+              <input
+                className="w-full"
+                name="choice2"
+                type="text"
+                value={wronganswer2}
+              />
+              <div className="pr-5"></div>
+            </div>
+          </div>
+          <div className="flex gap-3  ">
+            <div className="flex size-8 items-center justify-center rounded-full bg-blue-100 text-lg font-semibold text-blue-900">
+              {" "}
+              3
+            </div>
+            <div className="flex grow flex-col gap-2  ">
+              <input
+                className="w-full"
+                name="choice3"
+                type="text"
+                value={wronganswer3}
+              />
+              <div className="pr-5"></div>
+            </div>
+          </div>
+          <div className="flex gap-3  ">
+            <div className="flex size-8 items-center justify-center rounded-full bg-blue-100 text-lg font-semibold text-blue-900">
+              {" "}
+              4
+            </div>
+            <div className="flex grow flex-col gap-2  ">
+              <input
+                className="w-full"
+                name="correctAnswer"
+                type="text"
+                value={correctAnswer}
+              />
+              <div className="pr-5"></div>
+            </div>
+          </div>
+          <div className="flex justify-center px-10">
+            <Button type="submit" className="rounded-lg bg-blue-900 ">
+              submit
+            </Button>
+          </div>
+        </CardContent>
+      </Card>
+    </form>
+  );
+}
+
+async function quizAction(formData: FormData) {
+  "use server";
+  const question = formData.get("question")?.toString();
+  const choice1 = formData.get("choice1")?.toString();
+  const choice2 = formData.get("choice2")?.toString();
+  const choice3 = formData.get("choice3")?.toString();
+  const correctAnswer = formData.get("correctAnswer")?.toString();
+
+  if (!question) return;
+  if (!choice2) return;
+  if (!choice1) return;
+  if (!choice3) return;
+  if (!correctAnswer) return;
+
+  const id = generateId(7);
+  try {
+    await db
+      .insert(questionTable)
+      .values({ id, question, choice1, choice2, choice3, correctAnswer });
+    console.log("success");
+  } catch (err) {
+    console.log("there was an error pushing to the db please try again ", err);
+  }
+}
